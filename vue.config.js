7280a7d821fb7a558be050c19316a9aa2a20cbaf
Freddie Nelson <freddie0208@hotmail.com>
2020-12-02 17:35:30 +0000

    configure vue dev server

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  pages: {
+    index: {
+      entry: "client/src/main.js",
+      template: "client/public/index.html",
+    },
+  },
+  devServer: {
+    proxy: "http://localhost:3000",
+    contentBase: path.join(__dirname, "client/public"),
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "client/src"),
+      },
+    },
+  },
+};
